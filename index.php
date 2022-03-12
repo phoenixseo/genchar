b1b0fcf3ea5ce76876aaa89de8bb4dd78af74e5e
Frank Pfabigan <info@phoenixseo.de>
2022-03-12 23:06:47 +0100

    hunter, druid added

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
 # simple wow charname generator
 # wow charname rules: https://eu.battle.net/support/de/article/34530
 #
-$file_flags = FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES;
-
-$warrior_collection = file( './wowclass/warrior.txt', $file_flags );
-$warlock_collection = file( './wowclass/warlock.txt', $file_flags );
-
 function char_name( $syllables, &$collection ) {
-
-  static $used_pointer;
 
   if( $syllables == 0 ) {
     return;
@@ -18,20 +11,13 @@ function char_name( $syllables, &$collection ) {
   $min = 0;
   $max = sizeof( $collection ) - 1;
 
-  # $used_pointer = $pointer = rand( $min, $max );
+  # $pointer = rand( $min, $max );
   # bitwise notation, which is faster
-  $used_pointer = $pointer = rand( )&$max;
-
-  /*
-  if( $pointer == $used_pointer ) {
-    $pointer = rand( $min, $max );
-  }
-  */
+  $pointer = rand( )&$max;
 
   $namepart = $collection[$pointer];
 
   echo "<br>pointer: " . $pointer;
-  echo " used_pointer: " . $used_pointer;
   echo " namepart: " . $namepart;
 
 
@@ -40,7 +26,16 @@ function char_name( $syllables, &$collection ) {
   return $result;
 }
 
+$file_flags = FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES;
+
+$warrior_collection = file( './wowclass/warrior.txt', $file_flags );
+$warlock_collection = file( './wowclass/warlock.txt', $file_flags );
+$hunter_collection = file( './wowclass/hunter.txt', $file_flags );
+$druid_collection = file( './wowclass/druid.txt', $file_flags );
+
 # todo: trim name to 2-12 chars
 
-echo "<h2>warlock name = " . ucfirst( char_name( 10, $warlock_collection )) . "</h2>";
+echo "<h2>warlock name = " . ucfirst( char_name( 3, $warlock_collection )) . "</h2>";
 echo "<h2>warrior name = " . ucfirst( char_name( 3, $warrior_collection )) . "</h2>";
+echo "<h2>hunter name = " . ucfirst( char_name( 3, $hunter_collection )) . "</h2>";
+echo "<h2>druid name = " . ucfirst( char_name( 3, $druid_collection )) . "</h2>";
