33e70609d5fb25375e63d6d29d5c641f1e4225f5
Frank Pfabigan <info@phoenixseo.de>
2022-03-16 14:22:13 +0100

    new charname presentation

--- index.php
+++ index.php
@@ -16,15 +16,21 @@ $g = new GenChar;
     <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <div class="relative py-5 flex items-center justify-center lg:justify-between">
         <div class="absolute left-0 flex items-center flex-shrink-0 lg:static">
-          <a href="index.php">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-300.svg" alt="GenChar">
+          <a href="index.php" class="text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+            </svg>
           </a>
           <a href="index.php" class="text-white ml-3">GenChar</a>
         </div>
 
         <!-- Right section on desktop -->
         <div class="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
-          <a href="https://gens-der-allianz.de" class="text-indigo-100 text-sm font-medium rounded-md bg-white bg-opacity-0 px-3 py-2 hover:bg-opacity-10"> Gens der Allianz </a>
+          <a href="https://gens-der-allianz.de" class="flex items-center text-indigo-100 text-sm font-medium rounded-md bg-white bg-opacity-0 px-3 py-2 hover:bg-opacity-10"> <span class="mr-2">Gens der Allianz</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+            </svg>
+          </a>
         </div>
 
       </div>
@@ -68,32 +74,76 @@ $g = new GenChar;
                 </button>
                 </form>
 
+                <hr>
+
+
+
                 <?php
 
-                echo '<p class="text-xl">Warlock: ';
-                echo '<br><span class="text-5xl font-mono">'. $g->char_name( 'warlock', 3 ) . '</span>';
-                echo '</p>';
-                echo "<hr>";
+                echo '<div class="ring-2">';
+                  echo '<div class="flex items-center not-prose bg-gray-200">';
+                  echo $g->get_class_icon('warlock');
+                  echo '<span class="ml-2 text-2xl">Warlock</span>';
+                  echo '</div>';
 
-                echo '<p class="text-xl">Demonhunter: ';
-                echo '<br><span class="text-5xl font-mono">'. $g->char_name( 'demonhunter', 3 ) . '</span>';
-                echo '</p>';
-                echo "<hr>";
+                  echo '<div class="not-prose p-2">';
+                  echo '<p class="text-5xl font-mono">'. $g->char_name( 'warlock', 3 ) . '</p>';
+                  echo '</div>';
+                echo '</div>';
+                echo '<hr>';
 
-                echo '<p class="text-xl">Warrior: ';
-                echo '<br><span class="text-5xl font-mono">'. $g->char_name( 'warrior', 3 ) . '</span>';
-                echo '</p>';
-                echo "<hr>";
 
-                echo '<p class="text-xl">Druid: ';
-                echo '<br><span class="text-5xl font-mono">'. $g->char_name( 'druid', 3 ) . '</span>';
-                echo '</p>';
-                echo "<hr>";
+                echo '<div class="ring-2">';
+                  echo '<div class="flex items-center not-prose bg-gray-200">';
+                  echo $g->get_class_icon('demonhunter');
+                  echo '<span class="ml-2 text-2xl">Demonhunter</span>';
+                  echo '</div>';
 
-                echo '<p class="text-xl">Hunter: ';
-                echo '<br><span class="text-5xl font-mono">'. $g->char_name( 'hunter', 3 ) . '</span>';
-                echo '</p>';
-                echo "<hr>";
+                  echo '<div class="not-prose p-2">';
+                  echo '<p class="text-5xl font-mono">'. $g->char_name( 'demonhunter', 3 ) . '</p>';
+                  echo '</div>';
+                echo '</div>';
+                echo '<hr>';
+
+
+                echo '<div class="ring-2">';
+                  echo '<div class="flex items-center not-prose bg-gray-200">';
+                  echo $g->get_class_icon('warrior');
+                  echo '<span class="ml-2 text-2xl">Warrior</span>';
+                  echo '</div>';
+
+                  echo '<div class="not-prose p-2">';
+                  echo '<p class="text-5xl font-mono">'. $g->char_name( 'warrior', 3 ) . '</p>';
+                  echo '</div>';
+                echo '</div>';
+                echo '<hr>';
+
+
+                echo '<div class="ring-2">';
+                  echo '<div class="flex items-center not-prose bg-gray-200">';
+                  echo $g->get_class_icon('druid');
+                  echo '<span class="ml-2 text-2xl">Druid</span>';
+                  echo '</div>';
+
+                  echo '<div class="not-prose p-2">';
+                  echo '<p class="text-5xl font-mono">'. $g->char_name( 'druid', 3 ) . '</p>';
+                  echo '</div>';
+                echo '</div>';
+                echo '<hr>';
+
+
+                echo '<div class="ring-2">';
+                  echo '<div class="flex items-center not-prose bg-gray-200">';
+                  echo $g->get_class_icon('hunter');
+                  echo '<span class="ml-2 text-2xl">Hunter</span>';
+                  echo '</div>';
+
+                  echo '<div class="not-prose p-2">';
+                  echo '<p class="text-5xl font-mono">'. $g->char_name( 'hunter', 3 ) . '</p>';
+                  echo '</div>';
+                echo '</div>';
+                echo '<hr>';
+
 
 
                 ?>
@@ -107,19 +157,33 @@ $g = new GenChar;
         <!-- Right column -->
         <div class="grid grid-cols-1 gap-4">
           <section aria-labelledby="section-2-title">
-            <h2 class="sr-only" id="section-2-title">Section title</h2>
+            <h2 class="sr-only" id="section-2-title">GenChar Info</h2>
             <div class="rounded-lg bg-white overflow-hidden shadow">
               <div class="p-6 prose">
                 <h2>GenChar, a WOW Charname Generator</h2>
-                <p>WOW Charname Generator from <a href="https://gens-der-allianz.de">Gens-der-Allianz</a>, Allianz Fraction on Thrall.</p>
+                <p>WOW Charname Generator from
+                  <a href="https://gens-der-allianz.de" class="inline-flex items-center"><span class="mr-2">Gens-der-Allianz</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                    </svg></a>, Allianz Fraction on Thrall.
+                </p>
+                <hr>
                 <h2>Available Char Classes</h2>
                 <p>So far, these Char Classes are available: <strong>
                   <?php $g->print_classes(); ?>
                 </strong></p>
 
+                <hr>
                 <?php $g->print_stats( ); ?>
+                <hr>
 
-                <p>This Software Project is on GITHUB: <a href="https://github.com/phoenixseo/genchar">https://github.com/phoenixseo/genchar</a></p>
+                <p>This Software Project is on GITHUB:
+                  <a href="https://github.com/phoenixseo/genchar" class="flex items-center">
+                    <span class="mr-2">https://github.com/phoenixseo/genchar</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                    </svg>
+                  </a></p>
 
               </div>
             </div>
